cfc4623c51c29bc460f264c298efb4563ba9895d
Ivan-10k <155702468+Ivan-10k@users.noreply.github.com>
2026-07-17 21:58:17 -0500

    Corrigiendo bloqueo de CORS para Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
-// Importamos el archivo de rutas que ya configuraste en la carpeta 'routes'
 import authRoutes from './routes/authRoutes.js'; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 1. Middlewares globales
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json());
 
-// 2. Conexión del Enrutador (Arquitectura Limpia)
-// Le decimos que cualquier petición que empiece con '/api/auth' 
-// debe ser manejada por tu archivo authRoutes.js
 app.use('/api/auth', authRoutes);
 
 // 3. Encender el servidor
